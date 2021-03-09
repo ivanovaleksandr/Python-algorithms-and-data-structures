06d5a10d2d13fc08c416aa6d1916a64d329bf2d5
ivanovaleksandr <70534387+ivanovaleksandr@users.noreply.github.com>
2021-03-09 21:40:15 +0300

    Урок 7

---
+++ task2.py
@@ -0,0 +1,53 @@
+"""
+2. Отсортируйте по возрастанию методом слияния одномерный вещественный массив,
+заданный случайными числами на промежутке [0; 50). Выведите на экран исходный
+и отсортированный массивы.
+
+Хотя в примерах к уроку уже есть вариант реализации слияния, попробуйте предложить другой
+(придумать или найти)
+
+И попытаться сделать замеры на массивах разной длины: 10, 100, 1000, ...
+
+Пример:
+Введите число элементов: 5
+Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
+Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
+"""
+
+from random import uniform
+import operator
+
+while True:
+    def merge_sort(lst_obj, compare=operator.lt):
+        if len(lst_obj) < 2:
+            return lst_obj[:]
+        else:
+            middle = int(len(lst_obj) / 2)
+            left = merge_sort(lst_obj[:middle], compare)
+            right = merge_sort(lst_obj[middle:], compare)
+            return merge(left, right, compare)
+
+
+    def merge(left, right, compare):
+        result = []
+        i, j = 0, 0
+        while i < len(left) and j < len(right):
+            if compare(left[i], right[j]):
+                result.append(left[i])
+                i += 1
+            else:
+                result.append(right[j])
+                j += 1
+        while i < len(left):
+            result.append(left[i])
+            i += 1
+        while j < len(right):
+            result.append(right[j])
+            j += 1
+        return result
+
+
+    n = int(input('Введите число элементов: '))
+    lst = [uniform(0, 50) for _ in range(n)]
+    print(f'Исходный - {lst}')
+    print(f'Отсортированный - {merge_sort(lst)}')
